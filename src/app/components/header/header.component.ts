99317eb49a48b27e37ec53c2011ac1467ad3a9b0
matoufaily <mtoufaily@interform.group>
2024-06-28 15:41:50 +0300

    create header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  searchControl = new FormControl('');
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.searchControl.valueChanges
+      .pipe(
+        debounceTime(300), // wait 300ms after the last event before emitting last event
+        distinctUntilChanged() // only emit if value is different from previous value
+      )
+      .subscribe((query) => {
+        this.router.navigate([''], { queryParams: { search: query } });
+      });
+  }
+}
