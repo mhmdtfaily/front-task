070f5afc56c88a84267ba3689158776d5bd72dc3
matoufaily <mtoufaily@interform.group>
2024-06-28 15:42:51 +0300

    create user details component

---
+++ src/app/components/user-details/user-details.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/models/user.model';
+
+@Component({
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.css'],
+})
+export class UserDetailsComponent implements OnInit {
+  user?: User;
+
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      const userId = params['id'];
+      this.userService.getUser(userId).subscribe(
+        (user) => {
+          console.log(user);
+          this.user = user;
+        },
+        (error) => {
+          console.error('Error fetching user details: ', error);
+        }
+      );
+    });
+  }
+}
